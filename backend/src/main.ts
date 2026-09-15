0b19fe2ef5c5da0d76393a57e309a14b89b47d32
Matheus William <matheuswilliambr987@gmail.com>
2026-09-15 16:58:29 -0300

    feat: libera CORS para o frontend em desenvolvimento (#41)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
